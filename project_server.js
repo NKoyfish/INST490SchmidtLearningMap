9fda84c0bde9abe447929f31426830e7ffeae6bd
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2022-04-21 16:40:06 -0400

    Testing some routes

--- project_server.js
+++ project_server.js
@@ -16,27 +16,76 @@ app.use(express.static("./"));
 app.listen(port, () => console.log(`Server app listening on port ${port}!`));
 
 // API to update the local database with Google data
-app.get("/updateDatabase", (req, res) => {
+app.get("/pythonTest", async (req, res) => {
+  console.log("python");
+  const python = spawn("python", [
+    "./dynamoDB-scripts/test.py",
+    "node.js",
+    "python",
+  ]);
+  let dataToSend = "testttt";
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+  await python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    console.log(dataToSend);
+    res.send(`<p>Updated?${dataToSend}</p>`);
+  });
+});
+app.get("/updateDatabase", async (req, res) => {
   console.log("updateDatabase called");
-
   // Parameters passed in spawn -
   // 1. type_of_script
   // 2. list containing Path of the script and arguments for the script
+  // code 0 is success
   // var spawn = require("child_process").spawnSync;
-  var process = spawn("python", ["./update_database.py"]);
-  res.send(JSON.parse(process.stdout.toString()));
+
+  try {
+    // Parameters passed in spawn -
+    // 1. type_of_script
+    // 2. list containing Path of the script and arguments for the script
+    // var spawn = require("child_process").spawnSync;
+    var process = spawn("python", [
+      "./dynamoDB-scripts/updateDynamoDB_test2.py",
+    ]);
+    let dataToSend = "";
+    process.stdout.on("data", (data) => {
+      dataToSend += data.toString();
+    });
+    process.stdout.on("end", (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+      //console.log(dataToSend);
+      res.json({ data: dataToSend });
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // API to get ALL data from database and return it in JSON format.
-app.get("/getAllData", (req, res) => {
+app.get("/getAllData", async (req, res) => {
   console.log("getAllData called");
-
-  // Parameters passed in spawn -
-  // 1. type_of_script
-  // 2. list containing Path of the script and arguments for the script
-  // var spawn = require("child_process").spawnSync;
-  var process = spawn("python", ["./read_all_database.py"]);
-  res.send(JSON.parse(process.stdout.toString()));
+  try {
+    // Parameters passed in spawn -
+    // 1. type_of_script
+    // 2. list containing Path of the script and arguments for the script
+    // var spawn = require("child_process").spawnSync;
+    var process = spawn("python", ["./dynamoDB-scripts/readDynamoDB.py"]);
+    let dataToSend = "";
+    process.stdout.on("data", (data) => {
+      dataToSend += data.toString();
+    });
+    process.stdout.on("end", (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+      //console.log(dataToSend);
+      res.json({ data: dataToSend });
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // API to get some data from database and return it in JSON format.
